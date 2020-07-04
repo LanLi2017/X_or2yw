b5e9d27a5b19c24fa4f2515eae1dafb7e887998d
Lan Li <lilan.scut@gmail.com>
2020-07-04 15:48:08 -0500

    reverse data cleaning task

--- run.bash
+++ run.bash
@@ -1,3 +1,3 @@
 #python hybrid.py --root_path 1660451457167.project --log log
-#python hybrid.py --root_path copy1660451457167.project --log log1
-python hybrid.py --root_path 1603323347442.project --log log2
+python hybrid.py --root_path copy1660451457167.project --log log1
+#python hybrid.py --root_path 1603323347442.project --log log2
